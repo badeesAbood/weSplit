b195251fc9272ef2c682204c1c12ee920bc990d0
badees abood <57720463+badees99@users.noreply.github.com>
2024-09-30 22:29:31 +0200

    we split init

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var keyBoardfocust: Bool
+    var amountPerPerson : (total: Double , person: Double ) {
+        let people = Double(numberOfPeople + 2)
+        
+        let totalAmount = (checkAmount/100) * Double(tipPercentage) + checkAmount
+        let amountPerPerson = totalAmount / people
+        return (totalAmount , amountPerPerson)
+    }
+    
+    let tipPercentages = [5, 10 ,15 ,20 ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount" , value: $checkAmount , format: .currency(code: Locale.current.currency?.identifier  ?? "LYD")).keyboardType(.decimalPad).focused($keyBoardfocust)
+                    
+                    Picker("Number of People" , selection: $numberOfPeople){
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section("How much do you want to tip ?") {
+                    Picker("Tip Percentage" , selection: $tipPercentage){
+                        ForEach(0..<101 , id: \.self){
+                            Text($0 ,format: .percent)
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                
+                Section("Amount per Person") {
+                    Text(amountPerPerson.person , format:  .currency(code: Locale.current.currency?.identifier ?? "LYD"))
+                }
+                
+                Section("Total Amount"){
+                    Text(amountPerPerson.total, format: .currency(code: Locale.current.currency?.identifier ?? "LYD"))
+                }
+            }.navigationTitle("WeSplit").toolbar {
+                if keyBoardfocust {
+                    Button("Done") {
+                        keyBoardfocust = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
